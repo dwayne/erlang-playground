045e4f769aa36d0ca370bee16c794af7d088f2d7
Dwayne Crooks <me@dwaynecrooks.com>
2026-09-09 16:54:47 -0400

    Refactor flake.nix and update the inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  outputs = { self, flake-utils, nixpkgs }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system:
       let
         pkgs = import nixpkgs { inherit system; };
